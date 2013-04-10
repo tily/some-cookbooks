b64874494a7bf8db474f49de4badb96debcb1c60
tily <tily05@gmail.com>
2013-04-11 01:23:34 +0900

    add dependency.

--- some/metadata.rb
+++ some/metadata.rb
@@ -5,3 +5,4 @@ description      "Installs/Configures some"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          "hostsfile"
+depends          "haproxy"
